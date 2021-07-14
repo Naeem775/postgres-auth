6bb1a2a42b4091340f81d2a08ccb171e63997bda
rohaNoBorder <rohanaheed@noborders.net>
2021-07-14 18:38:52 +0500

    delete user and update password api added

--- Routes/user.js
+++ Routes/user.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const router = express.Router();
-const {getUser,signUp,login} = require('../controllers/user')
+const {getUser,signUp,login, updatePassword, deleteUser} = require('../controllers/user')
 
 router.get('/getUser', getUser);
 router.post('/signUp', signUp);
 router.put('/login', login);
+router.put('/updatePassword', updatePassword);
+router.put('/deleteUser', deleteUser);
 
 module.exports = router;
--- controllers/user.js
+++ controllers/user.js
@@ -2,25 +2,7 @@ const User = require('../Models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-// const sendTokenResponse = (user, statusCode, res) => {
-//     const token = user.getSignedJwtToken();
-//     const options = {
-//       expires: new Date(
-//         Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-//       ),
-//       httpOnly: false,
-//     };
 
-//     const data = {
-//       token,
-//       user,
-//     };
-//     res.status(statusCode).cookie('token', token, options).json({
-//       success: true,
-//       data,
-//       token,
-//     });
-//   };
 
 exports.getUser = async (req,res) => {
     const users = await  User.findAll();
@@ -28,6 +10,17 @@ exports.getUser = async (req,res) => {
         success:true,
         data:users
     })
+}
+
+exports.deleteUser = async (req,res) => {
+  console.log(req.body.email)
+  
+  const user = await User.findOne({where:{email:req.body.email}})
+  await user.destroy();
+  res.status(204).json({
+    success:true,
+    msg:"user deleted successfully"
+  });
 }
 
 exports.signUp = async (req,res) => {
@@ -50,7 +43,6 @@ exports.signUp = async (req,res) => {
 
 exports.login = async (req, res, next) => {
     const { email, password } = req.body;
-    let data;
     let user;
     user = await User.findOne({where:{ email:email }});
     console.log(user)
@@ -70,6 +62,37 @@ exports.login = async (req, res, next) => {
     
     res.status(200).json({
         success:true,
+        data:token
+    })
+  };
+
+  exports.updatePassword = async (req, res, next) => {
+    const { email, password } = req.body;
+  
+    let data;
+    let user;
+    user = await User.findOne({where:{ email:email }});
+    
+    if (!user) {
+      return res.status(401).json({success:false,data:'Invalid credentials'});
+    }
+    const passwordIsValid = bcrypt.compareSync(
+        req.body.password,
+        user.password
+      );
+        if (!passwordIsValid) {
+      return res.status(401).json({success:false,data:'Invalid credentials'});
+    }
+
+    user.password = bcrypt.hashSync(req.body.newPassword, 8)
+    await user.save();
+      const token = jwt.sign({ id: user.id },'my-test-secret', {
+        expiresIn: 86400 // 24 hours
+      });
+    
+    res.status(200).json({
+        success:true,
+        msg:"password updated successfully",
         data:token
     })
   };
